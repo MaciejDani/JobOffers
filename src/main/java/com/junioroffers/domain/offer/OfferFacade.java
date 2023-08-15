9caf869b1c8f169443c51e4683dba923dbc5c502
Uzytkownik <maciodan@gmail.com>
2023-08-15 10:50:06 +0200

    implemented offer module and tests

--- src/main/java/com/junioroffers/domain/offer/OfferFacade.java
+++ src/main/java/com/junioroffers/domain/offer/OfferFacade.java
@@ -1,4 +1,44 @@
 package com.junioroffers.domain.offer;
 
+import com.junioroffers.domain.offer.dto.OfferRequestDto;
+import com.junioroffers.domain.offer.dto.OfferResponseDto;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
 public class OfferFacade {
+
+    private final OfferRepository offerRepository;
+    private final OfferService offerService;
+
+    public List<OfferResponseDto> findAllOffers() {
+        return offerRepository.findAll()
+                .stream()
+                .map(OfferMapper::mapFromOfferToOfferDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<OfferResponseDto> fetchAllOffersAndSaveAllIfNotExists() {
+        return offerService.fetchAllOffersAndSaveAllIfNotExists()
+                .stream()
+                .map(OfferMapper::mapFromOfferToOfferDto)
+                .collect(Collectors.toList());
+    }
+
+    public OfferResponseDto findOfferById(String id) {
+        Optional<Offer> offer = offerRepository.findById(id);
+        if (offer.isEmpty()) {
+            throw new OfferNotFoundException(id);
+        }
+        return OfferMapper.mapFromOfferToOfferDto(offer.get());
+    }
+
+    public OfferResponseDto saveOffer(OfferRequestDto offerDto) {
+        final Offer offer = OfferMapper.mapFromOfferDtoToOffer(offerDto);
+        final Offer save = offerRepository.save(offer);
+        return OfferMapper.mapFromOfferToOfferDto(save);
+    }
 }
---
+++ src/main/java/com/junioroffers/domain/offer/OfferService.java
@@ -0,0 +1,39 @@
+package com.junioroffers.domain.offer;
+
+import com.junioroffers.domain.offer.dto.OfferRequestDto;
+import com.junioroffers.domain.offer.dto.OfferResponseDto;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class OfferService {
+
+    private final OfferFetchable offerFetcher;
+    private final OfferRepository offerRepository;
+
+    List<Offer> fetchAllOffersAndSaveAllIfNotExists() {
+       List<Offer> jobOffers = fetchOffers();
+       final List<Offer> offers = filterNotExistingOffers(jobOffers);
+       try {
+           return offerRepository.saveAll(offers);
+       } catch (OfferDuplicateException duplicateKeyException) {
+           throw new OfferSavingException(duplicateKeyException.getMessage(), jobOffers);
+       }
+    }
+
+    private List<Offer> filterNotExistingOffers(List<Offer> jobOffers) {
+        return jobOffers.stream()
+                .filter(offerDto -> !offerDto.offerUrl().isEmpty())
+                .filter(offerDto -> !offerRepository.existsByOfferUrl(offerDto.offerUrl()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Offer> fetchOffers() {
+        return offerFetcher.fetchOffers()
+                .stream()
+                .map(OfferMapper::mapFromJobOfferResponseToOffer)
+                .toList();
+    }
+}
